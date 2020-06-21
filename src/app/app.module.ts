a5f346de903af162c75be23ee8a55a2bce095009
Zaedul Islam <islam.zaedul@gmail.com>
2020-06-21 16:05:18 +0600

    Add DashboardComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
