2e66194be70a587b293d193cdf70a2670aca79e6
Zaedul Islam <islam.zaedul@gmail.com>
2020-06-21 20:22:29 +0600

    Add toolbar

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    selector: 'dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+    tabs: string[] = [
+        'About Me',
+        'Education',
+        'Experience',
+        'Projects',
+        'Certifications',
+        'Research',
+        'Awards',
+        'Blog',
+        'CV',
+        'Contact'
+    ];
+
 
 }
