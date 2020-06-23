1afd322b8e2c1e6d115cbef363ff3b867ca56974
Zaedul Islam <islam.zaedul@gmail.com>
2020-06-24 02:04:02 +0600

    Show education information in unordered list format

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -11,21 +11,21 @@ export class EducationComponent {
             title: 'Bachelor of Science (B.Sc.)',
             institution: 'American International University-Bangladesh (AIUB)',
             area: 'Computer Science & Engineering (CSE)',
-            graduationYear: '2018 (December)',
+            passingYear: '2018 (December)',
             result: '3.95 out of 4.00, last two semesters GPA (by chronological order): 4.00/4.00 & 3.94/4.00'
         },
         {
             title: 'Higher Secondary Certificate (HSC)',
             institution: 'Govt. Rajendra College, Faridpur',
             area: 'Science',
-            graduationYear: '2013',
+            passingYear: '2013',
             result: 'GPA 5.00'
         },
         {
             title: 'Secondary School Certificate (SSC)',
             institution: 'Faridpur Zilla School, Faridpur',
             area: 'Science',
-            graduationYear: '2011',
+            passingYear: '2011',
             result: 'GPA 5.00'
         }
     ];
