4a5602954312a251191abf26cbe1acd02fb964d5
Zaedul Islam <islam.zaedul@gmail.com>
2021-06-18 00:55:31 +0600

    Implement initial layout of the "Education" page

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -8,6 +8,15 @@ import { Component } from '@angular/core';
 export class EducationComponent {
     educations: any[] = [
         {
+            id: 4,
+            title: 'Master of Applied Science (MASc)',
+            institution: 'Concordia University,  Montreal, Quebec, Canada',
+            area: 'Quality Systems Engineering',
+            passingYear: 'Ongoing',
+            result: 'Yet to Publish'
+        },
+        {
+            id: 3,
             title: 'Bachelor of Science (B.Sc.)',
             institution: 'American International University-Bangladesh (AIUB)',
             area: 'Computer Science & Engineering (CSE)',
@@ -15,6 +24,7 @@ export class EducationComponent {
             result: '3.95 out of 4.00, last two semesters GPA (by chronological order): 4.00/4.00 & 3.94/4.00'
         },
         {
+            id: 2,
             title: 'Higher Secondary Certificate (HSC)',
             institution: 'Govt. Rajendra College, Faridpur',
             area: 'Science',
@@ -22,6 +32,7 @@ export class EducationComponent {
             result: 'GPA 5.00'
         },
         {
+            id: 1,
             title: 'Secondary School Certificate (SSC)',
             institution: 'Faridpur Zilla School, Faridpur',
             area: 'Science',
