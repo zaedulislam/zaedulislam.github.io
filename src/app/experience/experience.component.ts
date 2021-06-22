97be85f163485ee28e3ad4179c7b69e74401a801
Zaedul Islam <islam.zaedul@gmail.com>
2021-06-23 00:36:17 +0600

    Implement initial layout of the "Experience" page

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -6,23 +6,30 @@ import { Component } from '@angular/core';
     styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent {
+    professionalSectionHeader: string = 'Professional Experience';
+    academicSectionHeader: string = 'Academic Experience';
+    extracurricularActivities: string = 'Extracurricular Activities';
+
     experience: any = [
         {
-            designation: 'Software Engineer',
+            designation: 'Senior Software Engineer',
             company: {
                 name: 'Enosis Solutions',
                 url: 'https://www.enosisbd.com/'
             },
+            startDate:'1 January 2019',
+            endDate: 'Present',
             responsibilities: [
-                "Involved in the full development life cycle of Big Data project including requirements analysis, design, development, integration, testing, bug fixing and deployment support using Apache Spark ecosystem",
-                "Automated ETL Pipeline which takes customer's multi-gigabyte of unstructured source data (GZIP files) and yields to over 150 GB structured data (CSV files) in both AWS S3 and AWS Redshift",
-                "Automated the reporting infrastructure to run on ETL processed data monthly as well as per different criteria using AWS EMR clusters for analyzing the financial opportunity",
-                "Split the huge data into chunks by applying the Best Fit algorithm to ensure the best usage of the Amazon EMR clusters",
+                "Involved in the full development life cycle of Big Data project including requirements analysis,  design, development, integration, testing, bug fixing, and deployment support using Apache Spark ecosystem",
+                "Automated ETL Pipeline which takes customer’s multi-gigabyte of unstructured source data (GZIP files) and yields to over 150 GB of structured data (CSV files) in both AWS S3 and AWS Redshift",
+                "Automated  the  reporting  infrastructure  to  run  on  ETL  processed  data  monthly  as  well  as  per  different criteria using AWS EMR clusters for analyzing the financial opportunity",
+                "Split the huge data into chunks by applying the Best Fit algorithm to ensure the best usage of the AmazonEMR clusters",
                 "Did performance benchmarking (memory and process time) to optimize Spark jobs, each processing up to 30 millions of records in a Spark dataframe",
                 "Developed REST APIs in Scala using Play Framework and new user-facing features using Angular 2",
-                "Wrote and executed test scripts using ScalaTest in the Scala ecosystem",
+                "Wrote and executed test scripts using ScalaTestin the Scala ecosystem",
                 "Prepared documentation of use cases and flow charts to illustrate the entire workflow of several processes",
-                "Developed a highly interactive web application that uses the 'Internet of Things' by utilizing Angular 8, Angular Material Design, and integrating RESTful APIs"
+                "Developed a highly interactive web application that uses the \"Internet of Things\" by utilizing Angular 9, Angular Material Design, and integrating RESTful APIs",
+                "Work in a Scrum team"
             ]
         },
         {
@@ -31,6 +38,8 @@ export class ExperienceComponent {
                 name: 'American International University-Bangladesh',
                 url: 'https://www.aiub.edu/'
             },
+            startDate:'September 2018',
+            endDate: 'December 2018',
             responsibilities: [
                 "Assisted students to learn different topics of 'Artificial Intelligence and Expert System' and to implementthe source codes in the lab",
             ]
